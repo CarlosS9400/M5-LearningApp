046c29f8cce23f5771e93b0256c766578a9891b5
Carlos Santos <gotgoodjob@gmail.com>
2023-09-06 06:50:50 +0800

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Carlos Dos santos on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
